388c3883c4d96069d77646d34e893c906442d7b2
Aleksandar Makelov <aleksandar.makelov@gmail.com>
2022-07-06 16:20:34 -0400

    class-level comments for main components so far

--- mandala_lite/core.py
+++ mandala_lite/core.py
@@ -14,12 +14,20 @@ def get_uid() -> str:
 
 ################################################################################
 class ValueRef:
+    """
+    Wraps objects with storage metadata (uid for now). 
+    
+    This is the object passed between memoized functions (ops).
+    """
     def __init__(self, uid:str, obj:Any):
         self.uid = uid
         self.obj = obj
     
 
 class Hashing:
+    """
+    Helpers for hashing e.g. function inputs and call metadata.
+    """
     @staticmethod
     def content_hash(obj:Any) -> str:
         """
@@ -34,10 +42,21 @@ class Hashing:
 
 
 def wrap(obj:Any) -> ValueRef:
+    """
+    Wraps a value as a `ValueRef` (with uid = content hash) if it isn't one
+    already.
+    """
     return obj if isinstance(obj, ValueRef) else ValueRef(uid=Hashing.content_hash(obj), obj=obj)
 
 ################################################################################
 class Call:
+    """
+    Represents the inputs, outputs and uid of a call to an operation. 
+    
+    The inputs to an operation are represented as a dictionary, and the outputs
+    are a (possibly empty) list, mirroring how Python has named inputs but
+    nameless outputs for functions. This convention is followed throughout.
+    """
     def __init__(self, uid:str, inputs:Dict[str, ValueRef], 
                  outputs:List[ValueRef], op:'FuncOp'):
         self.uid = uid
@@ -48,6 +67,9 @@ class Call:
 
 ################################################################################
 class FuncOp:
+    """
+    Operation that models function execution.
+    """
     def __init__(self, func:Callable, version:int=0):
         self.func = func 
         self.sig = Signature.from_py(sig=inspect.signature(func), 
@@ -59,6 +81,19 @@ class FuncOp:
 
 
 class Signature:
+    """
+    Holds the metadata for a memoized function, which includes the function's
+    external and internal name, the external and internal input names (and the
+    mapping between them), the version, and the default values. Responsible for
+    manipulating this state and keeping it consistent.
+    
+    The internal name is an immutable ID that is used to identify the function
+    throughout its entire lifetime for the storage it is connected to. The
+    external name is what the user actually calls the function (it's the name of
+    the function object responsible for the semantics), and can be changed at
+    no significant computational cost.
+    
+    """
     def __init__(self, name:str, input_names:List[str], n_outputs:int, 
                  defaults:Dict[str, Any], version:int):
         self.name = name
@@ -111,7 +146,10 @@ class Signature:
         return Signature(name=name, input_names=input_names, n_outputs=n_outputs, defaults=defaults, version=version) 
 
 ################################################################################
-class KVStorage:
+class JoblibStorage:
+    """
+    A basic file-based key-value storage for any Python objects.
+    """
     def __init__(self, root:Path):
         self.root = root
 
@@ -131,8 +169,20 @@ class KVStorage:
         os.remove(path=self.get_obj_path(k=k))
 
 
+class InMemoryStorage:
+    """
+    An in-memory key-value store for any Python objects.
+    """
+    pass
+
 ################################################################################
 class RelStorage:
+    """
+    Responsible for the low-level (i.e., unaware of mandala-specific concepts)
+    interactions with the RDBMS part of the storage, such as creating and
+    extending tables, running queries, etc. This is intended to be a pretty
+    generic database interface supporting just the things we need.
+    """
     def create_relation(self, name:str, columns:List[str]):
         raise NotImplementedError()
     
@@ -153,6 +203,11 @@ class RelStorage:
 
 
 class RelAdapter:
+    """
+    Responsible for high-level RDBMS interactions, such as taking a bunch of
+    calls and putting their data inside the database; uses `RelStorage` to do
+    the actual work. 
+    """
     def __init__(self, rel_storage:RelStorage):
         self.rel_storage = rel_storage 
 
@@ -162,6 +217,13 @@ class RelAdapter:
 
 
 class SigStorage:
+    """
+    Keeps track of the operations connected to a given storage.
+    
+    Responsible for synchronizing a memoized function with the storage, making
+    sure the interface of the function you pass is compatible with what's on
+    record.
+    """
     def __init__(self):
         # (external name, version) -> signature
         self.sigs:Dict[Tuple[str, int], Signature] = {}
@@ -184,17 +246,35 @@ class SigStorage:
 
 ################################################################################
 class Storage:
+    """
+    Groups together all the components of the storage system. 
+    
+    Responsible for things that require multiple components to work together,
+    e.g. moving calls from the "temporary" partition to the "main" partition.
+    """
     def __init__(self, root:Path):
         self.root = root
-        self.call_storage = KVStorage(root=self.root / 'calls') 
-        self.obj_storage = KVStorage(root=self.root / 'objs') 
+        # where calls committed in the DB are
+        self.calls_main = JoblibStorage(root=self.root / 'calls_main') 
+        # where calls are first added
+        self.calls_temp = JoblibStorage(root=self.root / 'calls_temp') 
+        # all objects (inputs and outputs to operations) are saved here
+        self.obj_storage = JoblibStorage(root=self.root / 'objs') 
+        # stores the memoization tables
         self.rel_storage = RelStorage()
+        # manipulates the memoization tables
         self.rel_adapter = RelAdapter(rel_storage=self.rel_storage) 
+        # stores the signatures of the operations connected to this storage
         self.sig_storage = SigStorage()
 
 
 ################################################################################
 class FuncInterface:
+    """
+    Wrapper around a memoized function. 
+    
+    This is the object the `@op` decorator converts functions into.
+    """
     def __init__(self, op:FuncOp, storage:Storage):
         self.op = op
         self.storage = storage
@@ -218,9 +298,9 @@ class FuncInterface:
         call_uid = Hashing.content_hash(obj=[{k: v.uid for k, v in wrapped_inputs.items()},
                                              self.op.sig.internal_name])
         # check if call UID exists in call storage
-        if self.storage.call_storage.exists(call_uid):
+        if self.storage.calls_main.exists(call_uid):
             # get call from call storage
-            call = self.storage.call_storage.get(call_uid)
+            call = self.storage.calls_main.get(call_uid)
             # get outputs from obj storage
             outputs = {k: self.storage.obj_storage.get(v.uid) for k, v in call.outputs.items()}
             # return outputs and call
@@ -233,7 +313,7 @@ class FuncInterface:
             # create call
             call = Call(uid=call_uid, inputs=wrapped_inputs, outputs=wrapped_outputs, op=self.op)
             # set call in call storage
-            self.storage.call_storage.set(k=call_uid, v=call)
+            self.storage.calls_main.set(k=call_uid, v=call)
             # set outputs in obj storage
             for v in outputs:
                 self.storage.obj_storage.set(v.uid, v)
@@ -251,6 +331,9 @@ class FuncInterface:
 
         
 class FuncDecorator:
+    """
+    This is the `@op` decorator internally
+    """
     def __init__(self, storage:Storage):
         self.storage = storage
          
