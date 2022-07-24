b904ce69f03d0b05d9adb441ce1fbdd86f6c086b
Aleksandar Makelov <aleksandar.makelov@gmail.com>
2022-07-24 23:07:44 +0300

    some more tests for queries

--- mandala_lite/tests/test_queries.py
+++ mandala_lite/tests/test_queries.py
@@ -74,10 +74,49 @@ def test_superops_multilevel():
     @op(storage)
     def add_many(xs:List[int], ys:List[int]) -> List[int]:
         result = []
-        for x in xs:
-            for y in ys:
-                result.append(add(x, y))
+        for x in unwrap(xs):
+            for y in unwrap(ys):
+                result.append(add(wrap(x), wrap(y)))
         return result
     
     with run(storage):
         result = add_many(xs=wrap([1, 2, 3]), ys=wrap([4, 5, 6]))
+    check_invariants(storage=storage)
+
+    # individual adds
+    with query(storage) as q:
+        x, y = Q().named("x"), Q().named("y")
+        z = add(x, y).named('z')
+        df = q.get_table(x, y, z)
+    assert all(df['x'] + df['y'] == df['z'])
+    assert set(zip(df['x'].values.tolist(), df['y'].values.tolist())) == set(itertools.product([1, 2, 3], [4, 5, 6]))
+    
+    # end-to-end
+    with query(storage) as q:
+        xs, ys = Q().named("xs"), Q().named("ys")
+        result = add_many(xs=xs, ys=ys).named('result')
+        df = q.get_table(xs, ys, result)
+    assert df['xs'].item() == [1, 2, 3]
+    assert df['ys'].item() == [4, 5, 6]
+    assert [unwrap(x) for x in df['result'].item()] == [a + b for a in [1, 2, 3] for b in [4, 5, 6]]
+    
+    # two levels of nesting
+    @op(storage)
+    def add_many_many(xs:List[int], ys:List[int], zs:List[int]) -> List[int]:
+        intermediate = add_many(xs, ys)
+        final = add_many(intermediate, zs)
+        return final
+    
+    with run(storage):
+        a = wrap([1, 2, 3])
+        b = wrap([4, 5, 6])
+        c = wrap([7, 8, 9])
+        d = add_many_many(xs=a, ys=b, zs=c)
+    
+    with query(storage) as q:
+        xs, ys, zs = Q().named("xs"), Q().named("ys"), Q().named("zs")
+        intermediate = add_many(xs, ys).named('intermediate')
+        final = add_many(intermediate, zs).named('final')
+        df = q.get_table(xs, ys, intermediate, final)
+        assert len(df['intermediate'].item()) == 9
+        assert len(df['final'].item()) == 27
