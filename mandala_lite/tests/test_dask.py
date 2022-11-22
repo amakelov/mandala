8bfb2ed6462fbb0b53fcf771323a6b103ebaefbf
Aleksandar Makelov <aleksandar.makelov@gmail.com>
2022-11-22 18:01:02 +0200

    import dask for tests only if present

--- mandala_lite/tests/test_dask.py
+++ mandala_lite/tests/test_dask.py
@@ -1,46 +1,47 @@
 from mandala_lite.all import *
 from mandala_lite.tests.utils import *
-import dask
-import pytest
-from dask.distributed import Client
-import time
 
-DB_PATH = Path(__file__).parent / "output/dask.db"
+if Config.has_dask:
+    import dask
+    import pytest
+    from dask.distributed import Client
+    import time
 
+    DB_PATH = Path(__file__).parent / "output/dask.db"
 
-def _test_computation():
-    if DB_PATH.exists():
-        DB_PATH.unlink()
-    try:
-        client = Client(n_workers=4)
-        storage = Storage(multiproc=True, db_path=DB_PATH)
-
-        @op(executor="dask")
-        def f(x: int) -> int:
-            time.sleep(1)
-            return x + 1
-
-        @op(executor="dask")
-        def g(x: int) -> int:
-            time.sleep(1)
-            return x - 1
-
-        @op(executor="dask")
-        def h(x: int, y: int) -> int:
-            time.sleep(1)
-            return x + y
-
-        for func in [f, g, h]:
-            storage.synchronize(f=func)
-
-        with storage.run():
-            futures = []
-            for i in range(10):
-                futures.append(h(f(i), g(i)))
-            results = dask.compute(*futures)
-            time.sleep(10)
-    except:
-        pass
-    finally:
+    def test_computation():
         if DB_PATH.exists():
             DB_PATH.unlink()
+        try:
+            client = Client(n_workers=4)
+            storage = Storage(multiproc=True, db_path=DB_PATH)
+
+            @op(executor="dask")
+            def f(x: int) -> int:
+                time.sleep(1)
+                return x + 1
+
+            @op(executor="dask")
+            def g(x: int) -> int:
+                time.sleep(1)
+                return x - 1
+
+            @op(executor="dask")
+            def h(x: int, y: int) -> int:
+                time.sleep(1)
+                return x + y
+
+            for func in [f, g, h]:
+                storage.synchronize(f=func)
+
+            with storage.run():
+                futures = []
+                for i in range(10):
+                    futures.append(h(f(i), g(i)))
+                results = dask.compute(*futures)
+                time.sleep(10)
+        except:
+            pass
+        finally:
+            if DB_PATH.exists():
+                DB_PATH.unlink()
