f7b567bfdd4cee333306c2b38075de583f64c5c4
Aleksandar Makelov <aleksandar.makelov@gmail.com>
2023-01-06 16:23:56 +0200

    fix the mess

--- mandala/imports.py
+++
@@ -1,9 +0,0 @@
-"""
-Intended way for users to import mandala
-"""
-from .core.model import wrap
-from .core.wrapping import unwrap
-from .core.weaver import BuiltinQueries
-from .core.config import Config
-from .ui.main import Storage, Context, OnChange
-from .ui.funcs import op, superop, Q
